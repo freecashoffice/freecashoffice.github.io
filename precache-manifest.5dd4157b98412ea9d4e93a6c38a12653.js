3eb8c489e929916598102e371292df1daee89761
freecashoffice <freecashoffice@protonmail.com>
2021-01-30 22:15:50 +0800

    First Update

---
+++ precache-manifest.5dd4157b98412ea9d4e93a6c38a12653.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3974eb401956125ce635",
+    "url": "/css/app.7bcf12bc.css"
+  },
+  {
+    "revision": "2ec89669e8fb478a5bbc",
+    "url": "/css/chunk-358d9921.76eaf96f.css"
+  },
+  {
+    "revision": "b7453f58d255db115e38",
+    "url": "/css/chunk-3bfe1324.414301ba.css"
+  },
+  {
+    "revision": "5285d139e57d1fc2ce9f",
+    "url": "/css/chunk-3fb2baba.c43646ae.css"
+  },
+  {
+    "revision": "86194891bc3211df64ba",
+    "url": "/css/chunk-41cd37c3.df4be53c.css"
+  },
+  {
+    "revision": "71bf5b9be929c3b921a3",
+    "url": "/css/chunk-4f1b4790.5a5e6b89.css"
+  },
+  {
+    "revision": "36731fe4ef6b71f999fb",
+    "url": "/css/chunk-5efc62e8.4e72af3b.css"
+  },
+  {
+    "revision": "e250223246dfc70850ef",
+    "url": "/css/chunk-6518e1da.4e55c030.css"
+  },
+  {
+    "revision": "87e4c6c455a370c8009f",
+    "url": "/css/chunk-65e5654a.15c303fb.css"
+  },
+  {
+    "revision": "938788ce5b569567472c",
+    "url": "/css/chunk-67b5933c.1776d3ca.css"
+  },
+  {
+    "revision": "481f32636ede753fe3cf",
+    "url": "/css/chunk-6df79710.f19a8b90.css"
+  },
+  {
+    "revision": "b2c2e8805da278a7edf982a586bd6fdb",
+    "url": "/img/award-bg.b2c2e880.png"
+  },
+  {
+    "revision": "0e8c60a545c0dac34bd29edde0b233c1",
+    "url": "/img/bg1.0e8c60a5.png"
+  },
+  {
+    "revision": "91fcdd3f45afa4e0914945c5722d3cb1",
+    "url": "/img/bg2.91fcdd3f.png"
+  },
+  {
+    "revision": "20bb2880dafcc9b80e3be64c893def20",
+    "url": "/img/bg3.20bb2880.png"
+  },
+  {
+    "revision": "c1656009323dee42f647c1dc71ed3d4e",
+    "url": "/img/bitpie.c1656009.png"
+  },
+  {
+    "revision": "66d7094531485a42c590bf64ddd3ad48",
+    "url": "/img/circle1.66d70945.png"
+  },
+  {
+    "revision": "960cf8538d138ece1a10882d466a974f",
+    "url": "/img/circle2.960cf853.png"
+  },
+  {
+    "revision": "6bdb482f06328b625ff91995b7df3aa0",
+    "url": "/img/circle3.6bdb482f.png"
+  },
+  {
+    "revision": "dd14438a48276c6ec1e34fe0d4902aa3",
+    "url": "/img/circle4.dd14438a.png"
+  },
+  {
+    "revision": "27b008d32a4b774a32825ca976d493a9",
+    "url": "/img/circle5.27b008d3.png"
+  },
+  {
+    "revision": "cdb38f8de8aa26f69b381dff3f594f7f",
+    "url": "/img/circle6.cdb38f8d.png"
+  },
+  {
+    "revision": "07027a3f95e8ccb16bb1d08096b5a2cc",
+    "url": "/img/circle7.07027a3f.png"
+  },
+  {
+    "revision": "cbad4bec68967f3c2f7b99195fac7d85",
+    "url": "/img/diamond.cbad4bec.png"
+  },
+  {
+    "revision": "a4addb446f9fe79445d8a17c5aaee943",
+    "url": "/img/dt.a4addb44.png"
+  },
+  {
+    "revision": "f31c06413d5eaa0fd5bf10bc73732c2c",
+    "url": "/img/flower.f31c0641.png"
+  },
+  {
+    "revision": "7b1a25b2e8b7d34c524792f4012ee9c3",
+    "url": "/img/gameRuleBg.7b1a25b2.png"
+  },
+  {
+    "revision": "eb91b190eaba455492935f29e3b925ed",
+    "url": "/img/highest.eb91b190.png"
+  },
+  {
+    "revision": "f7acdd0916bd11c78db276cb9342c008",
+    "url": "/img/home_luckplayer.f7acdd09.png"
+  },
+  {
+    "revision": "c5e7e41478c09b668b96d7b3fe1323b4",
+    "url": "/img/imtoken.c5e7e414.png"
+  },
+  {
+    "revision": "029cc5f5b81b4d413000a4a38ca7bfed",
+    "url": "/img/logo.029cc5f5.png"
+  },
+  {
+    "revision": "5566675d8425de35b19d65f2fc09caba",
+    "url": "/img/lv0.5566675d.png"
+  },
+  {
+    "revision": "2327641c0436b5a1b62f2ef78e27e995",
+    "url": "/img/lv1.2327641c.png"
+  },
+  {
+    "revision": "70674aea88ba02e5836dcd0ae3cd6408",
+    "url": "/img/lv2.70674aea.png"
+  },
+  {
+    "revision": "d06c4bcd0caa7baaafb44909f38e63f1",
+    "url": "/img/lv3.d06c4bcd.png"
+  },
+  {
+    "revision": "d22da1727a17cbf1d86a6910f9891030",
+    "url": "/img/lv4.d22da172.png"
+  },
+  {
+    "revision": "cd42e29f729f8c691ea470aa67e1aac1",
+    "url": "/img/math.cd42e29f.png"
+  },
+  {
+    "revision": "f552b170ef9f25e8dc749c086cf6775c",
+    "url": "/img/metamask.f552b170.png"
+  },
+  {
+    "revision": "475155b60066ad7f02dc97683ea282ac",
+    "url": "/img/startLogo.475155b6.png"
+  },
+  {
+    "revision": "3e180e7a9f3637a774fb7dc32a469892",
+    "url": "/img/treasureChest.3e180e7a.png"
+  },
+  {
+    "revision": "3c56b2d8eec43bd6dc72085574045837",
+    "url": "/img/trust.3c56b2d8.png"
+  },
+  {
+    "revision": "010450456fdb3bb69d01b8d604bbe82d",
+    "url": "/img/usdt.01045045.png"
+  },
+  {
+    "revision": "e340237d2c35cd66217c31f454f62c3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3974eb401956125ce635",
+    "url": "/js/app.5ff07775.js"
+  },
+  {
+    "revision": "2ec89669e8fb478a5bbc",
+    "url": "/js/chunk-358d9921.88bb83b7.js"
+  },
+  {
+    "revision": "b7453f58d255db115e38",
+    "url": "/js/chunk-3bfe1324.bc54a15a.js"
+  },
+  {
+    "revision": "5285d139e57d1fc2ce9f",
+    "url": "/js/chunk-3fb2baba.00cc9a9b.js"
+  },
+  {
+    "revision": "86194891bc3211df64ba",
+    "url": "/js/chunk-41cd37c3.f7764267.js"
+  },
+  {
+    "revision": "71bf5b9be929c3b921a3",
+    "url": "/js/chunk-4f1b4790.25cbcc9c.js"
+  },
+  {
+    "revision": "36731fe4ef6b71f999fb",
+    "url": "/js/chunk-5efc62e8.1a3d685a.js"
+  },
+  {
+    "revision": "e250223246dfc70850ef",
+    "url": "/js/chunk-6518e1da.1a7a3629.js"
+  },
+  {
+    "revision": "87e4c6c455a370c8009f",
+    "url": "/js/chunk-65e5654a.a0a25c6a.js"
+  },
+  {
+    "revision": "938788ce5b569567472c",
+    "url": "/js/chunk-67b5933c.a38d811e.js"
+  },
+  {
+    "revision": "481f32636ede753fe3cf",
+    "url": "/js/chunk-6df79710.d4ba8891.js"
+  },
+  {
+    "revision": "8e285c771b389f6483ac",
+    "url": "/js/chunk-vendors.7f55873c.js"
+  },
+  {
+    "revision": "3d039d53e375abb98478cdf83925ab90",
+    "url": "/manifest.json"
+  }
+]);
